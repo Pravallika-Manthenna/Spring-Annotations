198d8e26a5b9598188227b2f710a47cd0c491d2f
Pravallika-Manthenna <mslpravallika6@gmail.com>
2019-07-14 18:15:51 +0530

    annotations task1

---
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -0,0 +1,22 @@
+package com.stackroute.domain;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+
+public class Actor {
+
+  @Value("${actor.name}")
+  String name;
+  @Value("${actor.gender}")
+  String gender;
+  @Value("${actor.age}")
+  int age;
+
+
+
+  public void display(){
+    System.out.println("actor details: "+ "\n" + name + "\n" + gender + "\n"  + age);
+  }
+}
---
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -0,0 +1,24 @@
+package com.stackroute.domain;
+
+import org.springframework.stereotype.Component;
+
+@Component
+  public class Movie {
+
+
+    Actor actor;
+
+  public Movie(Actor actor) {
+    this.actor = actor;
+  }
+
+  public void setActor(Actor actor) {
+      this.actor = actor;
+    }
+    public void displayActor()
+    {
+      actor.display();
+    }
+
+  }
+
