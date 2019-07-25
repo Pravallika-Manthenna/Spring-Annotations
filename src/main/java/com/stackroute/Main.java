c10f1d295aebdcc4aa3666fa74a8201b83d5d485
Pravallika-Manthenna <51950359+Pravallika-Manthenna@users.noreply.github.com>
2019-07-25 15:33:34 +0530

    Update Main.java

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,7 +1,4 @@
 package com.stackroute;
-
-
-
 import com.stackroute.domain.Movie;
 import com.stackroute.domain.SpringConfig;
 import org.springframework.context.ApplicationContext;
@@ -9,11 +6,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
   public  static  void main(String[] args) {
+    //replacing beans.xml with config class
     ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
     Movie movie = context.getBean("movieBean", Movie.class);
     movie.displayActor();
-
-
-
   }
 }
