d6bac6ff55178094f37ed43a8bcfccefd3efa4d2
Pravallika-Manthenna <51950359+Pravallika-Manthenna@users.noreply.github.com>
2019-07-26 06:32:52 +0530

    Update Actor.java

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Actor {
 
+  //using value annotation to get the value of the variable
   @Value("${actor.name}")
   String name;
   @Value("${actor.gender}")
