f41797784acef5b95efa572887e5f116c5eef34e
Pravallika-Manthenna <51950359+Pravallika-Manthenna@users.noreply.github.com>
2019-07-26 06:38:53 +0530

    Update SpringConfig.java

--- src/main/java/com/stackroute/domain/SpringConfig.java
+++ src/main/java/com/stackroute/domain/SpringConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 
+//configuration annotation
+//property source is used to get the values from the property file
 @Configuration
 @ComponentScan("com.stackroute.domain")
 @PropertySource("classpath:Actor-info.properties")
